8e392c3b3966686fb42683f21c2f38e9c9ffb3dc
Matilda Sjöblom <matilda.sjo96@gmail.com>
2018-05-15 11:23:14 +0200

    a bit on the way maybe tihi

--- Model.java
+++ Model.java
@@ -36,13 +36,29 @@ class Model {
     void step(double deltaT) {
         // TODO this method implements one step of simulation with a step deltaT
         for (Ball b : balls) {
-            // detect collision with the border
-            if (b.x < b.radius || b.x > areaWidth - b.radius || collision()) {
+
+            if(detectCollision()){
+                //vinkeln till nya planet
+                double beta = getAngle();
+
+                //rotera till nya planet
+                rotate(beta);
+
+                //räkna ut ny velocity
+                calculate();
+
+                //rotera tillbaka
+                rotate(-beta);
+            }
+            // detect detectCollision with the border
+            if (b.x < b.radius || b.x > areaWidth - b.radius) {
                 b.vx *= -1; // change direction of ball
             }
-            if (b.y < b.radius || b.y > areaHeight - b.radius || collision()) {
+            if (b.y < b.radius || b.y > areaHeight - b.radius) {
                 b.vy *= -1;
             }
+
+
 
 
 
@@ -55,7 +71,7 @@ class Model {
         }
     }
 
-    private boolean collision(){
+    private boolean detectCollision(){
         Ball b1 = balls[0];
         Ball b2 = balls[1];
 
@@ -70,6 +86,54 @@ class Model {
         double distanceYsquare = (b1.y - b2.y)*(b1.y - b2.y);
         double totalDiff = distanceYsquare + distanceXsquare;
         return totalDiff < (b1.radius + b2.radius)*(b1.radius + b2.radius);
+    }
+
+
+    //vinkeln mellan hypotenusan och vanliga x-axeln
+    private double getAngle(){
+        double x = Math.abs(balls[0].x - balls[1].x);
+        double y = Math.abs(balls[0].y - balls[1].y);
+        double h = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        return (Math.acos(x/h));
+    }
+
+    private void rotate(double beta){
+        //rotera x och y till ny dimension?
+
+        //gamla vx och vy (boll 1)
+        double vx11 = balls[0].vx;
+        double vy11 = balls[0].vy;
+
+        //gamla vx och vy (boll 2)
+        double vx21 = balls[1].vx;
+        double vy21 = balls[1].vy;
+
+        //nya vx och vy (boll 1)
+        balls[0].vx = Math.cos(beta)*vx11 - Math.sin(beta)*vy11;
+        balls[0].vy = Math.sin(beta)*vx11 + Math.cos(beta)*vy11;
+
+        //nya vx och vy (boll 2)
+        balls[1].vx = Math.cos(beta)*vx21 - Math.sin(beta)*vy21;
+        balls[1].vy = Math.sin(beta)*vx21 + Math.cos(beta)*vy21;
+    }
+
+
+    private void calculate(){
+        double u1 = balls[0].vx;
+        double u2 = balls[1].vx;
+        double m1 = balls[0].radius;
+        double m2 = balls[1].radius;
+
+        //kalkulera nya v1 och v2?????????????????++
+        double v1 = ((m1 - m2)*u1 + (2*m2*u2))/(m1 + m2);
+        double v2 = ((m2 - m1)*u2 + (2*m1*u1))/(m1 + m2);
+
+        //haha sätt nya vx och vy
+
+        balls[0].vx = v1;
+        balls[1].vx = v2;
+
+
     }
 
     /**
